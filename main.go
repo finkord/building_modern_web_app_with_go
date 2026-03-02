a1ad61971306f0ad167ee1eb4a1d508b5633fb89
finkord <devfinkord@gmail.com>
2026-03-02 22:19:00 +0200

    go3

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
-func main() {
-	var myString string
-	myString = "Green"
+var s = "seven"
 
-	log.Println("myString is set to", myString)
-
-	changeUsingPointer(&myString)
-
-	log.Println("after func call myString is set to", myString)
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
 }
 
-func changeUsingPointer(s *string) {
-	newValue := "Red"
+func main() {
 
-	*s = newValue
+	user := User{
+		FirstName:   "Trevor",
+		LastName:    "Sawler",
+		PhoneNumber: "1 555-555-1212",
+	}
+
+	log.Println(user.FirstName, user.LastName, "BirthDate", user.BirthDate, "Age", user.Age)
+
 }
