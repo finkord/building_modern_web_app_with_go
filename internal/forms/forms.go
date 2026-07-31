5c800ee5512d2826b9b3def46320fd403e9e742b
finkord <devfinkord@gmail.com>
2026-07-31 06:52:11 +0300

    video 68

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,14 +1,21 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form is a struct that contains the form data and errors.
 type Form struct {
 	url.Values
 	Errors errors
+}
+
+// Valid checks if the form is valid.
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
 }
 
 // New initializes a new form.
@@ -19,6 +26,16 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Required checks if the form has the required fields.
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field is required")
+		}
+	}
+}
+
 // Has checks if a field exists in the form.
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
@@ -26,5 +43,15 @@ func (f *Form) Has(field string, r *http.Request) bool {
 		return false
 	}
 
+	return true
+}
+
+// MinLength checks if a field has the minimum length.
+func (f *Form) MinLength(field string, lenght int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < lenght {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", lenght))
+		return false
+	}
 	return true
 }
---
+++ internal/models/models.go
@@ -0,0 +1,9 @@
+package models
+
+// Reservation is a struct that holds the reservation data.
+type Reservation struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+}
