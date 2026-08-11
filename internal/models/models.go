62720755f3fc6056935b53ec2e60de023829aaa8
finkord <devfinkord@gmail.com>
2026-08-11 03:03:37 +0300

    video 101

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,69 @@
 package models
 
+import "time"
+
 // Reservation is a struct that holds the reservation data.
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// Users is a struct that holds the user data.
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is a struct that holds the room data.
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is a struct that holds the room restriction data.
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is a struct that holds the reservation data.
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestriction is a struct that holds the room restriction data.
+type RoomRestriction struct {
+	ID            int
+	RoomID        int
+	RestrictionID int
+	ReservationID int
+	StartDate     time.Time
+	EndDate       time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
