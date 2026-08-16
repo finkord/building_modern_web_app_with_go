1e344a00ac0840b11a15967740c34e5be0fa3d08
finkord <devfinkord@gmail.com>
2026-08-16 05:08:49 +0300

    video 155

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -48,6 +48,7 @@ func routes() http.Handler {
 		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
 		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 		mux.Get("/process-reservations/{src}/{id}", handlers.Repo.AdminProcessReservation)
+		mux.Get("/delete-reservations/{src}/{id}", handlers.Repo.AdminDeleteReservation)
 
 		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminShowReservation)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
