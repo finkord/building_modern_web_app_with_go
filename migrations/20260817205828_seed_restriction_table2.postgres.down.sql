5178f79869548385473502f879c63e9b144332c7
finkord <devfinkord@gmail.com>
2026-08-19 03:19:59 +0300

    video 181

--- migrations/20260817205828_seed_restriction_table2.postgres.down.sql
+++
@@ -1,1 +0,0 @@
-delete from restrictions where restrictions_name = 'Tablyat';
--- migrations/20260817205828_seed_restriction_table2.postgres.up.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.restrictions (restrictions_name,created_at,updated_at) VALUES
-	 ('Tablyat','2026-08-17 00:00:00.000','2026-08-18 00:00:00.000');
